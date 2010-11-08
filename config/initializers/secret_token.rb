6d2bbcb51cc2ff9ce4ab73ed6ee13631b4f2091e
thilgner <git@das-buch-freigericht.de>
2010-11-08 10:59:41 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DASBUCH::Application.config.secret_token = 'ff56b250645f7380202995983c2c56eb9f03006ea6e345bbdfaf33a1fa75994b04f31df7e7bb2b125605724989f2d8bc980240c5a9034303069124995f5f80ac'
